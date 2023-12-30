8559a0e53f645c08b65ee5a65c319b2cf979cd46
alipoureskandar <alipoureskandar@outlook.com>
2023-12-30 12:36:25 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
+import { useState } from "react";
+import "./index.css";
+
+export default function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  function addTask() {
+    if (task.trim() !== "") {
+      setTasks([...tasks, { id: Date.now(), text: task, isComplete: false }]);
+      setTask("");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddTaskForm task={task} onSetTask={setTask} onAddTask={addTask} />
+      <TasksList tasks={tasks} onSetTasks={setTasks} />
     </div>
   );
 }
 
-export default App;
+function AddTaskForm({ task, onSetTask, onAddTask }) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    onAddTask();
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <label>Add the task to do: </label>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => onSetTask(e.target.value)}
+      ></input>
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+function TasksList({ tasks, onSetTasks }) {
+  return (
+    <ul className="task-container">
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} onSetTasks={onSetTasks} tasks={tasks} />
+      ))}
+    </ul>
+  );
+}
+
+function Task({ task, onSetTasks, tasks }) {
+  const [isComplete, setIsComplete] = useState(task.isComplete);
+
+  function toggleTaskCompletion() {
+    setIsComplete(!isComplete);
+  }
+
+  function deleteTask(id) {
+    onSetTasks(tasks.filter((currentTask) => currentTask.id !== id));
+  }
+
+  return (
+    <li className={`task ${isComplete ? "completed" : ""}`}>
+      {task.text}
+      {isComplete ? null : (
+        <>
+          <button onClick={toggleTaskCompletion}>✅</button>
+          <button onClick={() => deleteTask(task.id)}>❌</button>
+        </>
+      )}
+    </li>
+  );
+}
